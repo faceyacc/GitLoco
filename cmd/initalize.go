8272ddf024ec2b26bc92a299441178f7b113fb89
Ty <justfacey@gmail.com>
2024-01-03 13:46:28 -0500

    Added test for creating HEAD file in .git directory.

--- cmd/initalize.go
+++ cmd/initalize.go
@@ -11,4 +11,11 @@ func initalizeGit() {
 			fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 		}
 	}
+
+	HeadFileContents := []byte("ref: refs/heads/master\n")
+	if err := os.WriteFile(".git/HEAD", HeadFileContents, 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+	}
+
+	// fmt.Println("Initialized git directory")
 }
--- cmd/initalize_test.go
+++ cmd/initalize_test.go
@@ -6,15 +6,26 @@ import (
 )
 
 func TestInitalizeGit(t *testing.T) {
+	dirs := []string{".git", ".git/objects", ".git/refs"}
 	t.Run("initalize .git directory", func(t *testing.T) {
 		initalizeGit()
-
-		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		for _, dir := range dirs {
 			if _, err := os.Stat(dir); os.IsNotExist(err) {
 				t.Error(".git directory or subdirectories were not created")
 			}
 			defer os.RemoveAll(dir)
 		}
+	})
 
+	t.Run("create HEAD file", func(t *testing.T) {
+		initalizeGit()
+		HEADfile := ".git/HEAD"
+		for _, dir := range dirs {
+			defer os.RemoveAll(dir)
+		}
+
+		if _, err := os.Stat(HEADfile); os.IsNotExist(err) {
+			t.Error("HEAD file was not created")
+		}
 	})
 }
